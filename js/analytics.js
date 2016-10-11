7f7d18514dc97c919dc6066ec36b514cdff1611e
Andrei Cioara <andrei@cioara.me>
2016-10-10 20:17:35 -0500

    added error handling for missing pictures

--- js/analytics.js
+++ js/analytics.js
@@ -1,4 +1,4 @@
-(function() {
+var Analytics = (function() {
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -7,7 +7,11 @@
   ga('create', 'UA-55332519-1', 'auto');
   ga('send', 'pageview');
 
-  function handleOutboundLinkClicks(event) {
+  var socialImageErrorTriggered = false;
+  var socialImageErrorLogged = false;
+  var pageLoaded = false;
+
+  handleOutboundLinkClicks = function(event) {
     ga('send', 'event', {
       eventCategory: 'Outbound Link',
       eventAction: 'click',
@@ -16,7 +20,28 @@
     });
   }
 
+  handleSocialPictureError = function() {
+    if (socialImageErrorLogged) {
+      return;
+    }
+    ga('send', 'event', {
+      eventCategory: 'Error',
+      eventAction: 'Image',
+      eventLabel: 'Social',
+      transport: 'beacon'
+    })
+    socialImageErrorLogged = true;
+  }
+
+  sendSocialImageError = function() {
+    if (pageLoaded) {
+      handleSocialPictureError();
+    }
+    socialImageErrorTriggered = true;
+  }
+
   window.onload = function() {
+    pageLoaded = true;
     var anchors = document.getElementsByTagName("a"), item;
     for (var i = 0; i < anchors.length; i++) {
       item = anchors[i];
@@ -24,5 +49,13 @@
         item.onclick = handleOutboundLinkClicks;
       }
     }
+    if (socialImageErrorTriggered) {
+      handleSocialPictureError();
+    }
   }
+
+  return {
+    sendSocialImageError: sendSocialImageError
+  }
+
 })();
